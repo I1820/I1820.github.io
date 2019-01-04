77a1d600d1a547c5ab1d7f5d63647ae3c9cda6a0
Parham Alvani <parham.alvani@gmail.com>
2019-01-04 22:25:32 +0330

    Adds main module.

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { MainComponent } from './main.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { QuoteComponent } from './quote/quote.component';
+import { TeamComponent } from './team/team.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    HomeComponent,
+    AboutComponent,
+    QuoteComponent,
+    TeamComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [MainComponent]
+})
+export class MainModule { }
