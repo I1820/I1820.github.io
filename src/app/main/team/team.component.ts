f2f052776f6f3854df8fa0b30432edd2df98862f
Parham Alvani <parham.alvani@gmail.com>
2019-01-05 05:47:43 +0330

    Corrects lint issue.

--- src/app/main/team/team.component.ts
+++ src/app/main/team/team.component.ts
@@ -31,67 +31,67 @@ export class TeamComponent implements OnInit {
         github: 'https://github.com/1995parham',
       }
     }, {
-      name: "Iman Tabrizian",
+      name: 'Iman Tabrizian',
       roles: ['Core Developer'],
-      bio: "Docker Man",
-      avatar: "https://github.com/tabrizian.png",
-      url: "https://imantabrizian.me/",
+      bio: 'Docker Man',
+      avatar: 'https://github.com/tabrizian.png',
+      url: 'https://imantabrizian.me/',
       socials: {
-        github: "https://github.com/tabrizian"
+        github: 'https://github.com/tabrizian'
       }
     }, {
-      name: "Elahe Jalalpour",
+      name: 'Elahe Jalalpour',
       roles: ['Researcher'],
-      bio: "Who names the I1820",
-      avatar: "https://github.com/eljalalpour.png",
-      url: "http://ceit.aut.ac.ir/~jalalpour",
+      bio: 'Who names the I1820',
+      avatar: 'https://github.com/eljalalpour.png',
+      url: 'http://ceit.aut.ac.ir/~jalalpour',
       socials: {
-        github: "https://github.com/eljalalpour",
+        github: 'https://github.com/eljalalpour',
       }
     }, {
-      name: "Sepehr Sabour",
+      name: 'Sepehr Sabour',
       roles: ['Frontend Developer'],
-      bio: "A Friend",
-      avatar: "https://github.com/pesehr.png",
-      url: "https://pesehr.github.io/",
+      bio: 'A Friend',
+      avatar: 'https://github.com/pesehr.png',
+      url: 'https://pesehr.github.io/',
       socials: {
-        github: "https://github.com/pesehr",
+        github: 'https://github.com/pesehr',
       }
     }, {
-      name: "Sajjad Rahnama",
+      name: 'Sajjad Rahnama',
       roles: ['Backend Developer'],
-      bio: "A Friend",
-      avatar: "https://github.com/sajjadrahnama7.png",
-      url: "http://sajjadrahnama7.github.io/",
+      bio: 'A Friend',
+      avatar: 'https://github.com/sajjadrahnama7.png',
+      url: 'http://sajjadrahnama7.github.io/',
       socials: {
-        github: "https://github.com/sajjadrahnama7",
+        github: 'https://github.com/sajjadrahnama7',
       }
     }, {
-      name: "Mohammad Hussein Tavakoli Bina",
+      name: 'Mohammad Hussein Tavakoli Bina',
       roles: ['Product Manager'],
-      bio: "A Manager",
-      avatar: "https://github.com/mhtb32.png",
-      url: "http://mhtb.ir/",
+      bio: 'A Manager',
+      avatar: 'https://github.com/mhtb32.png',
+      url: 'http://mhtb.ir/',
       socials: {
-        github: "https://github.com/mhtb32",
+        github: 'https://github.com/mhtb32',
       }
     }, {
-      name: "Mahtab Farrokh",
+      name: 'Mahtab Farrokh',
       roles: ['Intern'],
-      bio: "Aolab Member",
-      avatar: "https://github.com/mahtabfarrokh.png",
-      url: "",
+      bio: 'Aolab Member',
+      avatar: 'https://github.com/mahtabfarrokh.png',
+      url: '',
       socials: {
-        github: "https://github.com/mahtabfarrokh"
+        github: 'https://github.com/mahtabfarrokh'
       }
     }, {
-      name: "Ramtin Shakeri",
+      name: 'Ramtin Shakeri',
       roles: ['Intern'],
-      bio: "Aolab Member",
-      avatar: "https://github.com/RamtinSh7596.png",
-      url: "",
+      bio: 'Aolab Member',
+      avatar: 'https://github.com/RamtinSh7596.png',
+      url: '',
       socials: {
-        github: "https://github.com/RamtinSh7596",
+        github: 'https://github.com/RamtinSh7596',
       }
     }
   ];
