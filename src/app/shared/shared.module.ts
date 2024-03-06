75107d178649520ed76bef6bf63e4abc5c2f724a
Parham Alvani <parham.alvani@gmail.com>
2024-03-06 05:49:40 +0000

    feat: finally we have a build :dancer:

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ScrollToModule } from "@andrei4ik/ngx-scroll-to";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ScrollToModule.forRoot()],
-  exports: [ScrollToModule, NgbModule],
+  imports: [CommonModule],
+  exports: [NgbModule],
 })
 export class SharedModule {}
